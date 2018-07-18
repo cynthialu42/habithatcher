5f80e92abccc23631ceeee465eec1d040dbf3257
cynthialu42 <clu2334@gmail.com>
2018-07-18 15:31:33 -0500

    server.js done

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const routes = require('./routes');
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve static assets
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+}
+
+// Let Express use routes
+app.use(routes);
+
+
+// Connect to database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/habithatcher");
+
+// Start the API server
+app.listen(PORT, function(){
+    console.log(`App listening on port ${PORT}`);
+});
